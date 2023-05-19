576450c0dd89d5a1e6cf4d3a9b00f3072dce3aa0
MirianaMD <miriana.maricutu@student.upt.ro>
2023-05-19 12:33:42 +0300

    User commit

---
+++ src/main/java/org/example/baza/User.java
@@ -0,0 +1,84 @@
+package org.example.baza;
+
+public class User {
+    private String username;
+    private String password;
+    private String role;
+    private String name;
+    private String email;
+
+    public User(String username, String password, String role, String name, String email) {
+        this.username = username;
+        this.password = password;
+        this.role = role;
+        this.name = name;
+        this.email = email;
+    }
+
+    public User() {}
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+    public String getName(){
+        return name;
+    }
+
+    public void setName(String name){
+        this.name = name;
+    }
+
+    public String getEmail(){
+        return email;
+    }
+
+    public void setEmail(String email){
+        this.email = email;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        User user = (User) o;
+
+        if (username != null ? !username.equals(user.username) : user.username != null) return false;
+        if (password != null ? !password.equals(user.password) : user.password != null) return false;
+        if (role != null ? role.equals(user.role) : user.role != null) return false;
+        if (name != null ? name.equals(user.name) : user.name != null) return false;
+
+        return email != null ? email.equals(user.email) : user.email == null;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = username != null ? username.hashCode() : 0;
+        result = 31 * result + (password != null ? password.hashCode() : 0);
+        result = 31 * result + (role != null ? role.hashCode() : 0);
+        result = 31 * result + (name != null ? name.hashCode() : 0);
+        result = 31 * result + (email != null ? email.hashCode() : 0);
+        return result;
+    }
+}
